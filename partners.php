80bddc392b9fcefd956a525fc66d236717cd6fc7
Mike Key <mike@mk3y.com>
2019-03-23 16:14:55 -0600

    more partners added to the event

--- partners.php
+++ partners.php
@@ -57,16 +57,30 @@
                             <img class="lazy" data-src="https://cdn.powerwagonsinmoab.com/partner-logos/logo-opp.png">
                         </a>
 
+                        <!-- Squatchboxx -->            
+                        <a href="https://squatchboxx.com/" target="_blank">
+                            <img class="lazy" data-src="https://cdn.powerwagonsinmoab.com/partner-logos/logo-Squatchboxx.png">
+                        </a>
+
                         <!-- AEV -->
                         <a href="https://www.aev-conversions.com/" target="_blank">
                             <img class="lazy" data-src="https://cdn.powerwagonsinmoab.com/partner-logos/logo-aev.png">
                         </a>
                         
                         <!-- Maple Offroad -->
-                        <a href="#" target="_blank">
+                        <a href="http://mapleoffroad.com/" target="_blank">
                             <img class="lazy" data-src="https://cdn.powerwagonsinmoab.com/partner-logos/logo-maple.png">
                         </a>
 
+                        <!-- Rugged Radio -->
+                        <a href="https://www.ruggedradios.com/" target="_blank">
+                            <img class="lazy" data-src="https://cdn.powerwagonsinmoab.com/partner-logos/logo-rugged-radios.png">
+                        </a>            
+
+                        <!-- JM Rigging -->              
+                        <a href="https://www.jmrigging.com/product-page/7-16" target="_blank">
+                            <img class="lazy" data-src="https://cdn.powerwagonsinmoab.com/partner-logos/logo-jm-rig.png">
+                        </a>
                     </div>
                                 
                 </div>
@@ -83,7 +97,30 @@
                             <img class="lazy" data-src="https://cdn.powerwagonsinmoab.com/partner-logos/logo-u4wda.png">
                         </a>
 
+                        <!-- JT Brooks -->              
+                        <a href="http://www.jtbrooks.com/" target="_blank">
+                            <img class="lazy" data-src="https://cdn.powerwagonsinmoab.com/partner-logos/logo-jt-brooks.png">
+                        </a>
 
+                        <!-- Factor 55 -->              
+                        <a href="https://factor55.com/" target="_blank">
+                            <img class="lazy" data-src="https://cdn.powerwagonsinmoab.com/partner-logos/logo-f55.png">
+                        </a>
+
+                        <!-- Method -->              
+                        <a href="https://www.methodracewheels.com/" target="_blank">
+                            <img class="lazy" data-src="https://cdn.powerwagonsinmoab.com/partner-logos/logo-method.png">
+                        </a>
+
+                        <!-- Dixie 4Wheel -->              
+                        <a href="https://dixie4wheeldrive.com/" target="_blank">
+                            <img class="lazy" data-src="https://cdn.powerwagonsinmoab.com/partner-logos/logo-dixie.png">
+                        </a>
+
+                        <!-- Warn -->              
+                        <a href="https://www.warn.com/" target="_blank">
+                            <img class="lazy" data-src="https://cdn.powerwagonsinmoab.com/partner-logos/logo-warn.png">
+                        </a>
                     </div>
                                 
                 </div>
